966ebe701f4104f0cf14d515084ce9347b51663c
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-14 23:45:55 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -7,7 +7,7 @@ st.set_page_config(layout='wide')
 
 st.title('🚀 AI Analytics Platform')
 
-API_URL = 'https://your-backend.onrender.com/analyze/'
+API_URL = "https://your-backend-name.onrender.com/analyze/"
 
 uploaded_file = st.file_uploader(
     'Upload CSV or Excel',
